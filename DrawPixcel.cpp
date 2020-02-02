df1a74eb3a888c8afa87891e0a40ed59b7753a0d
Itinii12 <1212965@gmail.com>
2020-02-02 22:24:57 +0900

    メッセージ表示改善 キャラ管理用のクラス作製

--- DrawPixcel.cpp
+++ DrawPixcel.cpp
@@ -1,3 +1,7 @@
+//次の課題
+//地形生成と当たり判定
+
+
 #include "DxLib.h"
 
 //色の定義
@@ -16,38 +20,66 @@ enum FLAG_NAME{
 };
 bool FLAGS[FLAG_MAX];	//フラグ管理用の配列
 
+//キャラ情報管理クラス
+class Charactor {
+public:
+	int chara_pic;	//キャラ画像
+	int chara_x;	//キャラ座標(x)
+	int chara_y;	//キャラ座標(y)
+	Charactor()
+	{
+		chara_pic = 0;
+		chara_x = 0;
+		chara_y = 0;
+	}
+};
+
+//テストゲームで使うキャラクタ生成
+Charactor Obatyan;
+
 //任意のメッセージを表示する関数
-//次回課題:メッセージ表示した後に、メッセージをウィンドウだけ消す
-//+消すときにはZキーを押したときだけ消すようにし、連続でメッセージが出ないようにする。
 void disp_msg(const char* charname,const char* msg) {
 
-	DrawBox(30, 295, 100, 325, BLUE, TRUE);	//NameBoxを描画
+	//NameBoxを描画
+	DrawBox(30, 295, 100, 325, BLUE, TRUE);	
 	DrawBox(30, 295, 100, 325, WHITE, FALSE);
-	DrawBox(10, 320, 630, 470, BLUE, TRUE);	//MsgBoxを描画
+	//MsgBoxを描画
+	DrawBox(10, 320, 630, 470, BLUE, TRUE);	
 	DrawBox(10, 320, 630, 470, WHITE, FALSE);
 
-	DrawString(35, 300, charname, WHITE);	//キャラ名表示
-	DrawString(20, 340, msg, WHITE);		//メッセージ表示
+	// キャラ名表示
+	DrawString(35, 300, charname, WHITE);
+	//メッセージ表示
+	DrawString(20, 340, msg, WHITE);		
+	ScreenFlip();
+	//次のキー入力が早すぎると変な挙動になるのでちょっと待つ
+	WaitTimer(100);	
 
-	ScreenFlip();
-	WaitKey();
+	//Zキーが押されるまで表示し続ける
+	while (CheckHitKey(KEY_INPUT_Z) == 0) {
+		
+	}
+
+	//これだと全部一回消えちゃうので、キャラ画像を再表示させておく。
 	ClearDrawScreen();
+	DrawExtendGraph(0 + Obatyan.chara_x, 0 + Obatyan.chara_y, 30 + Obatyan.chara_x, 30 + Obatyan.chara_y,Obatyan.chara_pic, TRUE);
 	ScreenFlip();
+	//次のキー入力が早すぎると変な挙動になるのでちょっと待つ
+	WaitTimer(100);
 }
 
-// プログラムは WinMain から始まります
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	//変数定義部
-	int Picture = 0; //画像表示ハンドル
-	int x = 0;
-	int y = 0;
+	//ローカル変数定義部
 
-	ChangeWindowMode(TRUE);	//画面サイズをウィンドウモードに
+	//画面サイズをウィンドウモードに
+	ChangeWindowMode(TRUE);	
 	if (DxLib_Init() == -1){  
 		return -1;			  
 	}
-	SetDrawScreen(DX_SCREEN_BACK);	//描画画面を裏に
+	//描画画面を裏に
+	SetDrawScreen(DX_SCREEN_BACK);
 
 	//タイトル画面処理
 	LoadGraphScreen(0,0,"Picture/title.bmp",TRUE);
@@ -58,21 +90,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	ClearDrawScreen();
 	ScreenFlip();
 
+
 	//表示画像ハンドル読み込み
-	Picture = LoadGraph("Picture/walking6_oldwoman.png");
+	Obatyan.chara_pic= LoadGraph("Picture/walking6_oldwoman.png");
 
 	//メイン処理部開始
 	while (FLAGS[GAME_END] == 0) {
 		//キー処理部
-		if (CheckHitKey(KEY_INPUT_UP) == 1)     y -= 10;	//上キー
-		if (CheckHitKey(KEY_INPUT_DOWN) == 1)   y += 10;	//下キー
-		if (CheckHitKey(KEY_INPUT_RIGHT) == 1)  x += 10;	//右キー
-		if (CheckHitKey(KEY_INPUT_LEFT) == 1)   x -= 10;	//左キー
+		if (CheckHitKey(KEY_INPUT_UP) == 1)     Obatyan.chara_y -= 10;	//上キー
+		if (CheckHitKey(KEY_INPUT_DOWN) == 1)   Obatyan.chara_y += 10;	//下キー
+		if (CheckHitKey(KEY_INPUT_RIGHT) == 1)  Obatyan.chara_x += 10;	//右キー
+		if (CheckHitKey(KEY_INPUT_LEFT) == 1)   Obatyan.chara_x -= 10;	//左キー
 		//境界値処理
-		if (y <= 0)y = 0;
-		if (x <= 0)x = 0;
-		if (y >= 450)y = 450;
-		if (x >= 610)x = 610;
+		if (Obatyan.chara_y <= 0)Obatyan.chara_y = 0;
+		if (Obatyan.chara_x <= 0)Obatyan.chara_x = 0;
+		if (Obatyan.chara_y >= 450)Obatyan.chara_y = 450;
+		if (Obatyan.chara_x >= 610)Obatyan.chara_x = 610;
 
 		if (CheckHitKey(KEY_INPUT_Z) == 1) {				//Zキー
 			disp_msg("HANAKO", "こんにちは！\n2行のテストメッセージです！");
@@ -81,7 +114,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 		WaitTimer(10);
 		ClearDrawScreen();
-		DrawExtendGraph(0+x, 0+y, 30+x, 30+y, Picture, TRUE);
+		DrawExtendGraph(0+Obatyan.chara_x, 0+ Obatyan.chara_y, 30+ Obatyan.chara_x, 30+ Obatyan.chara_y, Obatyan.chara_pic, TRUE);
 		ScreenFlip();
 	}
 
